46568f8b4eeb2a7fa952386baf853714be2df48a
Daniel Schulz <faulolio@gmail.com>
2026-08-22 01:50:54 +0200

    devops: integration tests

---
+++ .jenkins/Jenkinsfile.groovy
@@ -0,0 +1,83 @@
+import org.jenkinsci.plugins.workflow.steps.FlowInterruptedException
+
+def assertValue(actual, expected, description) {
+    if (actual != expected) {
+        error "${description}: expected '${expected}', got '${actual}'"
+    }
+}
+
+def candidateImage() {
+    return "$DOCKER_NAMESPACE/$DOCKER_IMAGE"
+}
+
+def testEmptyProjectImport() {
+    docker.image(candidateImage()).inside() {
+        def setupExitCode = execStatus 'godot --version'
+        assertValue(setupExitCode, 0, 'Godot setup must succeed')
+
+        dir('empty-project') {
+            deleteDir()
+            catchError(
+                message: 'Empty project import test failed',
+                stageResult: 'FAILURE',
+                buildResult: 'FAILURE'
+            ) {
+                try {
+                    timeout(time: 10, unit: 'SECONDS') {
+                        def exitCode = execStatus 'godot --headless --verbose --quit --editor --import'
+                        assertValue(exitCode == 0, false, 'Import on an empty project must not return success')
+                    }
+                } catch (FlowInterruptedException ignored) {
+                    error 'Import on an empty project timed out'
+                }
+            }
+        }
+    }
+}
+
+pipeline {
+    agent none
+    parameters {
+        choice(
+            name: 'DOCKER_NAMESPACE',
+            choices: ['faulo', 'tmp'],
+            description: 'Docker image namespace to test'
+        )
+    }
+    options {
+        disableConcurrentBuilds()
+        disableResume()
+        disableRestartFromStage()
+        skipDefaultCheckout()
+    }
+    stages {
+        stage('Integration Tests') {
+            matrix {
+                axes {
+                    axis {
+                        name 'TEST_NODE'
+                        values 'Dende', 'Garl'
+                    }
+                    axis {
+                        name 'GODOT_VERSION'
+                        values '4.0', '4.1', '4.2', '4.3', '4.4', '4.5', '4.6', '4.7'
+                    }
+                }
+                agent {
+                    label "${TEST_NODE}"
+                }
+                stages {
+                    stage('Empty Project Import') {
+                        steps {
+                            script {
+                                fromEnvFile {
+                                    testEmptyProjectImport()
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
